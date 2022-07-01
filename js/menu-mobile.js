eb1a7c9815edaf804762a4c76fa24b187f2df9c4
efriebel <emma.friebel@denkwerk.com>
2022-07-01 16:58:35 +0200

    Mobile Nav & Menu open&close functionality

---
+++ js/menu-mobile.js
@@ -0,0 +1,20 @@
+//HAMBURGER
+const openButton = document.querySelector(".open-menu");
+console.log("open button", openButton)
+
+//KREUZ
+const closeButton = document.querySelector(".close-menu");
+
+//MOBILE NAV
+const mobileMenu = document.querySelector(".mobile-menu-content");
+
+
+openButton.addEventListener("click", ()=> {
+    openButton.classList.remove("show-mobile");
+    mobileMenu.classList.add("show-mobile");
+});
+
+closeButton.addEventListener("click", ()=> {
+    openButton.classList.add("show-mobile");
+    mobileMenu.classList.remove("show-mobile");
+});
