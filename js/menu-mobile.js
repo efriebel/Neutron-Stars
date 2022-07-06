1b455ddee9d51164715cfc91742eca72e98eed01
efriebel <emma.friebel@denkwerk.com>
2022-07-06 16:49:44 +0200

    navi transition animation

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,8 +1,8 @@
-//HAMBURGER
+//OPEN
 const openButton = document.querySelector(".open-menu");
 console.log("open button", openButton)
 
-//KREUZ
+//CLOSE
 const closeButton = document.querySelector(".close-menu");
 
 //MOBILE NAV
@@ -12,9 +12,11 @@ const mobileMenu = document.querySelector(".mobile-menu-content");
 openButton.addEventListener("click", ()=> {
     openButton.classList.remove("show-mobile");
     mobileMenu.classList.add("show-mobile");
+    mobileMenu.classList.add("mobile-menu-content-transition");
 });
 
 closeButton.addEventListener("click", ()=> {
     openButton.classList.add("show-mobile");
     mobileMenu.classList.remove("show-mobile");
 });
+
