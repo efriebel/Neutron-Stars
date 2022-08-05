6db044864d94184a87ec6035c70ea164f571e27c
efriebel <emma.friebel@denkwerk.com>
2022-08-05 12:11:22 +0200

    mobile header | accordion textbox + funtioning button, InfoPage

---
+++ js/info-accordion.js
@@ -0,0 +1,14 @@
+
+document.querySelectorAll('.arrow-info-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const accordionContent = button.nextElementSibling;
+
+        button.classList.toggle('arrow-info-button-active');
+
+        if (button.classList.contains('arrow-info-button-active')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+        } else {
+            accordionContent.style.maxHeight = 0;
+        }
+    });
+});
