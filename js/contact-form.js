d18b61ed46606215d7e9a10eacc8e368741b5dfa
efriebel <emma.friebel@denkwerk.com>
2022-11-14 11:32:27 +0100

    major update

--- js/contact-form.js
+++ js/contact-form.js
@@ -19,21 +19,16 @@ const isRequired = (value) => {
         console.log('false')
     }
 }
-//isRequired('ok');
-//isRequired('');
 
 const validateEmail = (email) => {
-    const test = String(email)
-        .toLowerCase()
-        .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
-    if (test === true){
+    if (email.value.match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )){
         console.log('true')
     }
     else{
         console.log('not true')
     }
 };
-validateEmail('okaaayy');
-validateEmail('emma.friebel@denkwerk.com');
+//validateEmail('okaaayy');
+//validateEmail('emma.friebel@denkwerk.com');
