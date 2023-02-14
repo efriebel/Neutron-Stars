3f42ae6372a5754b7ef0b4f005765a3421dedc8e
efriebel <emma.friebel@denkwerk.com>
2023-02-14 17:40:47 +0100

    all svgs added to file, ZoomStage2, StarMapPage

--- js/more-info.js
+++ js/more-info.js
@@ -8,3 +8,8 @@ moreInfo.forEach(link => {
         link.nextElementSibling.style.left = `${(linkPosition - offsetLeftPosition)}px`;
     });
 });
+
+//need solution for mobile breakpoints!
+//issue: if aligned to left, more info gets pushed out of frame,
+//need more info content to be centered to screen width
+//how?? >> to mid-day WEDNESDAY 15.02.
