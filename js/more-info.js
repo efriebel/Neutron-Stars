8117b68bc349884bb4b6b27b60d89583bcd04321
efriebel <emma.friebel@denkwerk.com>
2023-02-14 13:31:41 +0100

    more info content position fixed, desktop, InfoPage

--- js/more-info.js
+++ js/more-info.js
@@ -1,10 +1,10 @@
 const moreInfo = document.querySelectorAll('.more-info')
-const moreInfoContent = document.querySelectorAll('.more-info-content');
 
 moreInfo.forEach(link => {
     link.addEventListener('mouseenter',(e) => {
 
-        const linkPosition = link.getBoundingClientRect();
-        link.nextElementSibling.style.left = `${linkPosition.left}px`;
+        const offsetLeftPosition = e.relatedTarget.getBoundingClientRect().left
+        const linkPosition = link.getBoundingClientRect().left;
+        link.nextElementSibling.style.left = `${(linkPosition - offsetLeftPosition)}px`;
     });
 });
